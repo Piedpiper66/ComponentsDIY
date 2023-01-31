80a6cc8b524b65a7a4132cb5c74ef9729c5f11b9
rao <1353262774@qq.com>
2023-01-31 15:18:27 +0800

    add LoadingBall

---
+++ Vue/LoadingBall/DataLoading.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="flex justify-center">
+    <div
+      class="loader"
+      :class="{ show }"
+      :style="{ '--ball-color': color, '--ball-size': size }"
+    >
+      <div class="circle"></div>
+      <div class="circle"></div>
+      <div class="circle"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loading",
+  props: {
+    show: Boolean,
+    color: {
+      type: String,
+      default: "cornflowerblue",
+    },
+    size: {
+      type: String,
+      default: "10px",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.loader {
+  opacity: 0;
+  display: flex;
+  /* position: fixed;
+  bottom: 30px;
+  transition: all 0.3s ease-in;
+  left: 50%;
+  transform: translateX(-50%); */
+  /* --ball-color: cornflowerblue; */
+}
+
+.loader.show {
+  opacity: 1;
+}
+
+.circle {
+  background-color: var(--ball-color);
+  width: var(--ball-size);
+  height: var(--ball-size);
+  border-radius: 50%;
+  margin: 5px;
+  animation: myBounce 0.5s ease-in-out infinite;
+}
+
+.circle:nth-of-type(2) {
+  animation-delay: 0.1s;
+}
+
+.circle:nth-of-type(3) {
+  animation-delay: 0.2s;
+}
+
+@keyframes myBounce {
+  0%,
+  100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-10px);
+  }
+}
+</style>
---
+++ Vue/LoadingBall/README.md
@@ -0,0 +1,8 @@
+# LoadingBall
+
+| 属性  | 说明     | 类型    | 默认值           |
+| ----- | -------- | ------- | ---------------- |
+| show  | 是否显示 | boolean | false            |
+| color | 小球颜色 | string  | 'cornflowerblue' |
+| size  | 小球大小 | string  | '10px'           |
+
